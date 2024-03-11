035fb2b30d10a8b792e928a58756f17b91e026f3
pranavsinghal <pranavsinghal96@gmail.com>
2024-03-11 23:24:47 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {Protocols} from '@waku/sdk'
-import { LightNodeProvider } from "@waku/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,15 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <LightNodeProvider
-      options={{defaultBootstrap: true}}
-      protocols={[Protocols.Store, Protocols.Filter, Protocols.LightPush]}
-    >
-      
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-      
-      </LightNodeProvider>
   );
 }

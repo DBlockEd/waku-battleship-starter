5648462e40c968d197273ea68d04e9f6e175007e
pranavsinghal <pranavsinghal96@gmail.com>
2024-03-12 11:58:59 +0530

    first commit

--- src/app/join/[id]/page.tsx
+++ src/app/join/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { ContentPairProvider } from "@waku/react";
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 import Container from "../../components/Container";
@@ -12,17 +11,13 @@ const Page = () => {
     const roomId = searchParams.id as string;
 
     return (
-        <ContentPairProvider contentTopic={`/waku-battlship/${roomId}`}>
-            <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-            <div className="text-lg font-bold text-center">
-                Welcome, <span className="text-green-500">{username}</span> <br />
-                you have joined the room <span className="text-blue-500"> {roomId} </span>
-            </div>
-
-            <Container player={Player.p2} />
-
-            </div>
-        </ContentPairProvider>
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+        <div className="text-lg font-bold text-center">
+            Welcome, <span className="text-green-500">{username}</span> <br />
+            you have joined the room <span className="text-blue-500"> {roomId} </span>
+        </div>
+        <Container player={Player.p2} />
+        </div>
     )
 };
 
